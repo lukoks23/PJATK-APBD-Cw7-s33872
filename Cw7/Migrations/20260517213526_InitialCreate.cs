55898af12988c01e1b85d8185dac514806a288b3
Łukasz Policha <s33872@pjwstk.edu.pl>
2026-05-18 00:11:09 +0200

    zapomniane migrations

---
+++ Cw7/Migrations/20260517213526_InitialCreate.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Cw7.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ComponentManufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Abrreviation = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    FoundationDate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComponentManufacturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComponentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Abbreviation = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComponentTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PCs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Weight = table.Column<double>(type: "float(5)", nullable: false),
+                    Warranty = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PCs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Components",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "char(10)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ComponentManufacturersId = table.Column<int>(type: "int", nullable: false),
+                    ComponentTypesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Components", x => x.Code);
+                    table.ForeignKey(
+                        name: "FK_Components_ComponentManufacturers_ComponentManufacturersId",
+                        column: x => x.ComponentManufacturersId,
+                        principalTable: "ComponentManufacturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Components_ComponentTypes_ComponentTypesId",
+                        column: x => x.ComponentTypesId,
+                        principalTable: "ComponentTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PCComponents",
+                columns: table => new
+                {
+                    PCId = table.Column<int>(type: "int", nullable: false),
+                    ComponentCode = table.Column<string>(type: "char(10)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PCComponents", x => new { x.PCId, x.ComponentCode });
+                    table.ForeignKey(
+                        name: "FK_PCComponents_Components_ComponentCode",
+                        column: x => x.ComponentCode,
+                        principalTable: "Components",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PCComponents_PCs_PCId",
+                        column: x => x.PCId,
+                        principalTable: "PCs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ComponentManufacturers",
+                columns: new[] { "Id", "Abrreviation", "FoundationDate", "FullName" },
+                values: new object[,]
+                {
+                    { 1, "cos1", new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dell" },
+                    { 2, "cos2", new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dell1" },
+                    { 3, "cos2", new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dell2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ComponentTypes",
+                columns: new[] { "Id", "Abbreviation", "Name" },
+                values: new object[,]
+                {
+                    { 1, "CT", "ComponentType1" },
+                    { 2, "CT1", "ComponentType2" },
+                    { 3, "CT", "ComponentType3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PCs",
+                columns: new[] { "Id", "CreatedAt", "Name", "Stock", "Warranty", "Weight" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "PC1", 20, 2, 5.5 },
+                    { 2, new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "PC2", 10, 3, 2.5 },
+                    { 3, new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "PC3", 3, 1, 8.5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Components",
+                columns: new[] { "Code", "ComponentManufacturersId", "ComponentTypesId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { "0000000000", 1, 1, "Description0", "Component0" },
+                    { "0000000001", 2, 2, "Description1", "Component1" },
+                    { "0000000002", 1, 2, "Description2", "Component2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PCComponents",
+                columns: new[] { "ComponentCode", "PCId", "Amount" },
+                values: new object[,]
+                {
+                    { "0000000000", 1, 10 },
+                    { "0000000002", 1, 20 },
+                    { "0000000001", 2, 30 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_ComponentManufacturersId",
+                table: "Components",
+                column: "ComponentManufacturersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Components_ComponentTypesId",
+                table: "Components",
+                column: "ComponentTypesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PCComponents_ComponentCode",
+                table: "PCComponents",
+                column: "ComponentCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PCComponents");
+
+            migrationBuilder.DropTable(
+                name: "Components");
+
+            migrationBuilder.DropTable(
+                name: "PCs");
+
+            migrationBuilder.DropTable(
+                name: "ComponentManufacturers");
+
+            migrationBuilder.DropTable(
+                name: "ComponentTypes");
+        }
+    }
+}
